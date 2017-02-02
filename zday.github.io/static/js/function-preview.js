809048afe487b3588d765c096d78db5aed219cfb
PC <pc@MacBook-Air-PC.local>
2017-02-02 15:06:44 +0200

    16th try

--- zday.github.io/static/js/function-preview.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.variants-list .item').hover(function(){
-        $('.variants-list').addClass('hover');
-    },
-    function(){
-        $('.variants-list').removeClass('hover');
-    })
-})
